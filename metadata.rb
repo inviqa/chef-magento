c902c59615d45de3868c897c8c6075971ad6dd48
Alistair Stead <alistair.stead@designdisclosure.com>
2013-07-01 10:37:29 +0100

    Restore depends data but add comments with URLs
    These dependencies must be met by the Cheffile before this
    cookbook is included or dependencies will not be able to be resolved.
    This is because librarian will attempt to pull from the Opscode API to
    meet this internal dependency unless it is met already from another
    source.

--- metadata.rb
+++ metadata.rb
@@ -13,9 +13,9 @@ depends "database", "> 1.3.0"
 depends "apache2"
 depends "memcached"
 # depends "redis" # This Opscode cookbook has an error
-#depends "chef-varnish"
-depends "chef-magento"
-#depends "solr"
+depends "chef-varnish" # https://github.com/inviqa/chef-varnish
+depends "chef-magento" # https://github.com/inviqa/chef-magento
+depends "chef-solr" # https://github.com/inviqa/chef-solr
 
 %w{ ubuntu }.each do |os|
   supports os
