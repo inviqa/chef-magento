bf3aed3eab8c2fe339a2cc7eb2ad55e05e9f1a3b
Ben Longden <blongden@inviqa.com>
2013-10-08 14:17:22 +0100

    Allow configureable SSL details

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "alistair.stead@inviqa.com"
 license          "Apache 2.0"
 description      "Installs/Configures Magento"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 depends "php"
 depends "chef-php-extra"
