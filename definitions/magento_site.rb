97fcd93adf3b63d79cced4fb656182fa24529373
Daniel <danielkmariam@gmail.com>
2013-11-14 11:54:26 +0000

    creates apache configuration for a site

--- definitions/magento_site.rb
+++ definitions/magento_site.rb
@@ -1,23 +1,26 @@
 define :magento_site do
-  site = params[:site]
+  instance_resource params[:instance_resource]
 
-  web_app site['servername'] do
-    template "apache-vhost.conf.erb"
-    ssl false
-    apache node['apache']
-    php node['magento']['php']
-    site site
-    magento node['magento']
-    notifies :reload, resources("service[apache2]"), :delayed
+  site_data = {
+    "servername" => @params[:servername],
+    "run_code" => @params[:run_code],
+    "additional_rewrite" => @params[:additional_rewrite],
+    "server_alias" => @params[:server_alias]
+  }
+
+  def configure_apache(servername, ssl)
+    web_app servername do
+      template "apache-vhost.conf.erb"
+      ssl ssl
+      apache instance_resource.magento[:apache]
+      php instance_resource.magento[:php]
+      site site_data
+      magento instance_resource.magento
+      notifies :reload, resources("service[apache2]"), :delayed
+    end
   end
 
-  web_app "#{site['servername']}.ssl" do
-    template "apache-vhost.conf.erb"
-    ssl true
-    apache node['apache']
-    php node['magento']['php']
-    site site
-    magento node['magento']
-    notifies :reload, resources("service[apache2]"), :delayed
-  end
+  configure_apache(@params[:servername], false)
+  configure_apache("#{@params[:servername]}.ssl", true)
+
 end
