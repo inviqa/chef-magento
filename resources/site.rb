463a5a94d67be2c1c2abbd89ac8231512091fdee
Andy Thompson <athompson@inviqa.com>
2013-11-22 12:45:46 +0000

    Add an additional resource attribute for additional_config

--- resources/site.rb
+++ resources/site.rb
@@ -11,6 +11,7 @@ attribute :servername, :kind_of => String
 attribute :run_code, :kind_of => String
 attribute :docroot, :kind_of => String
 attribute :additional_rewites, :kind_of => String
+attribute :additional_config, :kind_of => String
 attribute :server_alias, :kind_of => Array
 attribute :apache_document_root, :kind_of => String
 attribute :newrelic_name, :kind_of => String
