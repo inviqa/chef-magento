31cbbeccb0e4394a354c051da948c3679f32a0cf
Barney Hanlon <barney@shrikeh.net>
2014-03-10 16:09:38 +0000

    Change SSL protocol to accept array of protocols. Update cipher to be variable with sane default

--- attributes/default.rb
+++ attributes/default.rb
@@ -44,6 +44,11 @@ default['magento']['apache']['enable_mmap'] = "On"
 default['magento']['apache']['enable_sendfile'] = "On"
 default['magento']['apache']['ssl']['keyfile'] = "ssl/magento.key"
 default['magento']['apache']['ssl']['certfile'] = "ssl/magento.pem"
+default['magento']['apache']['ssl']['protocols'] = [
+    "TLSv1", "TLSv1.1", "TLSv1.2"
+]
+default['magento']['apache']['ssl']['ciphersuite'] = "ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH
++3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS;"
 
 default['magento']['cronjob']['minute'] = "*/5"
 
