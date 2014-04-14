baa98c0f8d872b1a717ea26c53aebb50d77c197b
Shane Auckland <shane.auckland@gmail.com>
2014-04-14 09:50:31 +0100

    Allow configuration of probe and backend timeouts

--- attributes/default.rb
+++ attributes/default.rb
@@ -70,13 +70,17 @@ default['varnish']['cookies'] = ['currency', 'store']
 default['magento']['varnish']['backend_servers'] = [
     {
         "name" => "web1",
-        "ip" => "127.0.0.1"
+        "ip" => "127.0.0.1",
+        "connect_timeout" => '240s',
+        "first_byte_timeout" => '240s',
+        "between_bytes_timeout" => '240s'
     }
 ]
 default['magento']['varnish']['trusted_servers'] = [
     "127.0.0.1"
 ]
 default['magento']['varnish']['ttl_for_static_files'] = '30d'
+default['magento']['varnish']['probe']['timeout'] = '90s'
 default['magento']['varnish']['additional_vcls'] = []
 default['magento']['varnish']['additional_recv_subs'] = []
 default['magento']['varnish']['additional_hash_subs'] = []
