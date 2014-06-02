bd4eb1ad6b617a1fb1a1b23ae0fb0a9c68942c12
Alistair Stead <alistair.stead@designdisclosure.com>
2014-06-02 10:20:08 +0100

    Bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          "Apache 2.0"
 dirname          "chef-magento"
 description      "Installs/Configures Magento"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 depends "php"
 depends "chef-php-extra"
