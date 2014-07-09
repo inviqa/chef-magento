24c1c807d58a04fb804fdabee63d99ef66f6783e
Marco De Bortoli <marco.debo.debortoli@gmail.com>
2014-07-09 14:08:57 +0100

    Version needs to be increased becuase of the recent additions

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 maintainer       "Alistair Stead"
 maintainer_email "alistair.stead@inviqa.com"
 license          "Apache 2.0"
-name          "chef-magento"
+name             "chef-magento"
 description      "Installs/Configures Magento"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "1.0.4"
 
 depends "php"
 depends "chef-php-extra"
