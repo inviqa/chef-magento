28f5a27d4e50757c15b6e93f8d2e884188ca9446
Andy Thompson <athompson@inviqa.com>
2014-09-24 17:28:22 +0100

    Add name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 maintainer       "Alistair Stead"
 maintainer_email "alistair.stead@inviqa.com"
+name             "chef-magento"
 license          "Apache 2.0"
 description      "Installs/Configures Magento"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
