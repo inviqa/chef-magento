b3cac91034b9ed17ff9b375d7cc0991d01dec655
Ramsay Bell <ramsay@ramsaybell.com>
2015-09-03 13:59:39 +0100

    Change Magento cronjob to run every minute
    Magento EE Support have advised the Magento cronjob should run every minute: "Current versions of Magento are designed around the cron running every minute. If the cron is running less frequently you may see some undesirable behavior. We do not recommend that cron every five minutes."

--- attributes/default.rb
+++ attributes/default.rb
@@ -50,10 +50,10 @@ default['magento']['apache']['ssl']['certfile'] = "ssl/magento.pem"
 default['magento']['apache']['ssl']['protocols'] = "All -SSLv2 -SSLv3"
 default['magento']['apache']['ssl']['ciphersuite'] = "ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS;"
 
-default['magento']['cronjob']['minute'] = "*/5"
+default['magento']['cronjob']['minute'] = "*/1"
 default['magento']['cronjob']['user'] = 'apache'
 
-default['magento']['cronjob']['minute'] = "*/5"
+default['magento']['cronjob']['minute'] = "*/1"
 default['magento']['cronjob']['hour'] = "*"
 default['magento']['cronjob']['name'] = "magento-crontab"
 default['magento']['cronjob']['user'] = "root"
